229bb04470eef961e4710d1511fef665a2a1b22f
Catlinman <andrew.lake@amwasser.de>
2014-12-24 16:53:56 +0100

    Added original placeholder files

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <title>Hivecom</title>
+        <meta name="description" content="Hivecom's official website">
+        <meta name="keywords" content="hivecom,teamspeak,catlinman,jokler,trif,cobalt community,game server,voice server">
+        <meta name="author" content="Catlinman">
+        <meta charset="UTF-8">
+
+        <link rel="shortcut icon" href="/images/icon.png">
+        <link rel="stylesheet" type="text/css" href="/style.css">
+
+        <link href='http://fonts.googleapis.com/css?family=Aldrich' rel='stylesheet' type='text/css'>
+    </head>
+
+    <body>
+        <div class="divider"></div>
+        <div class="container">
+            <div class="bar"></div>
+            <header class="noclick">
+                HIVECOM
+            </header>
+            <h3>
+                This page is currently in development - to visit the Teamspeak server click <a href="ts3server://ts.hivecom.net">here</a>.
+            </h3>
+            <h3>
+                Created by <a href="http://catlinman.com">Catlinman</a>
+            </h3>
+            <div class="bar"></div>
+    </body>
+
+</html>
