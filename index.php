8a8093f01184dfacc0ba81def3c543fd8c7594fc
Catlinman <andrew.lake@amwasser.de>
2014-12-26 19:55:31 +0100

    Started working on the page layout

--- index.php
+++ index.php
@@ -1,33 +1,58 @@
 <!DOCTYPE html>
 <html>
 
-    <head>
-        <title>Hivecom</title>
-        <meta name="description" content="Hivecom's official website">
-        <meta name="keywords" content="hivecom,teamspeak,catlinman,jokler,trif,cobalt community,game server,voice server">
-        <meta name="author" content="Catlinman">
-        <meta charset="UTF-8">
+<head>
+    <title>Hivecom</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta name="description" content="Hivecom is an open gaming community for anyone to feel free to join - we also host a Teamspeak 3 server under the same name">
+    <meta name="keywords" content="hivecom,teamspeak,catlinman,jokler,trif,cobalt community,game server,voice server">
+    <meta name="author" content="Hivecom Team - Catlinman">
+    <meta name="viewport" content="width=540">
 
-        <link rel="shortcut icon" href="/images/icon.png">
-        <link rel="stylesheet" type="text/css" href="/style.css">
+    <meta itemprop="image" content="images/metaicon.png">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="@catlinman_">
+    <meta name="twitter:creator" content="@catlinman_">
+    <meta name="twitter:title" content="Hivecom">
+    <meta name="twitter:description" content="Hivecom is an open gaming community for anyone to feel free to join - we also host a Teamspeak 3 server under the same name.">
+    <meta name="twitter:image" content="http://hivecom.net/images/metaicon.png">
+    <meta name="twitter:domain" content="http://hivecom.net">
+    <meta name="twitter:url" content="http://hivecom.net">
+    <meta property="og:url" content="http://hivecom.net">
 
-        <link href='http://fonts.googleapis.com/css?family=Aldrich' rel='stylesheet' type='text/css'>
-    </head>
+    <link rel="shortcut icon" href="images/icon.png" type="image/x-icon">
+    <link rel="icon" href="images/icon.png" type="image/x-icon">
+    <link rel="stylesheet" type="text/css" href="/style.css">
+    <link rel="stylesheet" type="text/css" href='http://fonts.googleapis.com/css?family=Aldrich'>
 
-    <body>
-        <div class="divider"></div>
-        <div class="container">
-            <div class="bar"></div>
-            <header class="noclick">
-                HIVECOM
-            </header>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript" src="/analytics.min.js"></script>
+</head>
+
+<body>
+    <div id="wrapper">
+        <div id="headerbar">
             <h3>
-                This page is currently in development - to visit the Teamspeak server click <a href="ts3server://ts.hivecom.net">here</a>.
+                <a href="index.php">HIVECOM</a>
             </h3>
-            <h3>
-                Created by <a href="http://catlinman.com">Catlinman</a>
-            </h3>
-            <div class="bar"></div>
-    </body>
+        </div>
+        <div id="info-panel">
+            <h2>
+                Welcome to Hivecom
+            </h2>
+            <p>
+                - Official website of the Hivecom Teamspeak server -
+            <p>
+        </div>
+        <nav id="navmenu">
+            <a href="/ts.php" id="tsbutton" alt="Connect to the Hivecom Teamspeak server">CONNECT USING TEAMSPEAK</a>
+            <a href="#" alt="Server information">THE SERVER</a>
+            <a href="#" alt="The people behind Hivecom">ABOUT US</a>
+        </nav>
+        <footer>
+
+        </footer>
+    </div>
+</body>
 
 </html>
