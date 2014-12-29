0fb9cfc273da636fb3fc36b7f858642f7f8fcb6e
Catlinman <andrew.lake@amwasser.de>
2014-12-29 01:14:16 +0100

    Added better access error handling

---
+++ errors/notfound.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Hivecom - 404</title>
+    <?php include_once($_SERVER['DOCUMENT_ROOT']. "/resources/head.php");?>
+</head>
+
+<body class="errorbody">
+    <div id="wrapper">
+        <header id="top">
+            <div id="headerinfo">
+                <h3>
+                    <a href="index.php">HIVECOM</a>
+                </h3>
+            </div>
+        </header>
+        <div id="titlediv" class="">
+            <h2>
+                Page not found
+            </h2>
+            <p style="margin:170px 0">
+                <a href="/index.php">Click here to return to the main page</a>
+            <p>
+        </div>
+    </div>
+</body>
+
+</html>
