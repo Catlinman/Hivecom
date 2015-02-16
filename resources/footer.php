ca157d85fcf457cc63abf047a40c73946eeec31e
Catlinman <andrew.lake@amwasser.de>
2015-02-16 20:40:47 +0100

    Edited the footer link to fit the new repository name

--- resources/footer.php
+++ resources/footer.php
@@ -2,7 +2,7 @@
     <div id="footercontainer" class="shadowed">
         <a href="http://catlinman.com/" alt="Catlinman homepage">The Hivecom website was created by Catlinman - &copy Catlinman <?php echo date("Y"); ?></a>
         <br>
-        <a href="https://github.com/Catlinman/Hivecom" style="font-size:12px" alt="Hivecom on GitHub">View the source code on GitHub</a>
+        <a href="https://github.com/Catlinman/hivecom.net" style="font-size:12px" alt="Hivecom on GitHub">View the source code on GitHub</a>
     </div>
 </footer>
 <script>
